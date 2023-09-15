349456539875f164c768264af0c22e56aa6dabbf
YoshiTv-Development <142677543+YoshiTv-Development@users.noreply.github.com>
2023-09-15 20:34:49 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+name 'Crafting'
+description 'Crafting System With Ox'
+author 'YoshiTv'
+version '1.0'
+discord 'https://discord.gg/pGP37hb5rF'
+
+shared_script {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua',
+    'shared/**.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
---
+++ server/server.lua
@@ -0,0 +1,34 @@
+-------------------------------------------------------------------------------------------------
+------------------------------------- PARTE SERVER ARMI -----------------------------------------
+-------------------------------------------------------------------------------------------------
+RegisterServerEvent("pistola", function(item)
+    exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
+    exports.ox_inventory:AddItem(source, 'WEAPON_PISTOL', Config.QuantitaRicevere, false, false)
+end)
+
+RegisterServerEvent("pistolamk2", function(item)
+    exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
+    exports.ox_inventory:AddItem(source, 'WEAPON_PISTOL_MK2', Config.QuantitaRicevere, false, false)
+end)
+
+RegisterServerEvent("pdw", function(item)
+    exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
+    exports.ox_inventory:AddItem(source, 'WEAPON_COMBATPDW', Config.QuantitaRicevere, false, false)
+end)
+-------------------------------------------------------------------------------------------------
+------------------------------------ PARTE SERVER OGGETTI ---------------------------------------
+-------------------------------------------------------------------------------------------------
+RegisterServerEvent("granata", function(item)
+    exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
+    exports.ox_inventory:AddItem(source, 'WEAPON_GRENADE', Config.QuantitaRicevere, false, false)
+end)
+
+RegisterServerEvent("armour", function(item)
+    exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
+    exports.ox_inventory:AddItem(source, 'ARMOUR', Config.QuantitaRicevere, false, false)
+end)
+
+RegisterServerEvent("parachute", function(item)
+    exports.ox_inventory:RemoveItem(source, Config.Item.oggetto, item, false, false)
+    exports.ox_inventory:AddItem(source, 'PARACHUTE', Config.QuantitaRicevere, false, false)
+end)
